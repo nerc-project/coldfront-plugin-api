bcf287ae686e52532bee281337e6971c3a6467aa
Quan Pham <qmpham2019@gmail.com>
2023-12-15 15:10:52 -0500

    Implemented fix command

--- src/coldfront_plugin_api/management/commands/fix_allocation_users.py
+++ src/coldfront_plugin_api/management/commands/fix_allocation_users.py
@@ -7,26 +7,42 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = "A Fix command that adds users assigned to allocations to the project that those allocations belong to"
+    help = "A command that adds users who have been assigned allocations to those allocations' project. Made to fix a \
+    database inconsistency where a user is assigned to an allocation, but may not have been assigned to the allocation's project. \
+    Run this command with no options to see the list of users with this inconsistency. Run with '--apply' to fix them."
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--apply",
+            action="store_true", 
+            help="Applies changes to database to fix all allocation user inconsistencies"
+        )
+        return 0
+    
     def handle(self, *args, **options):
-        self.stdout.write(
-                self.style.SUCCESS('TESTING FIX COMMAND')
-            )
         
-        # Check all allocations
+        # Check all allocation users
         for a_user in AllocationUser.objects.filter(status__name="Active"):
+
             user = a_user.user
-            p_user = ProjectUser.objects.filter(user=user)
-            if not p_user:
+            project = a_user.allocation.project
+            p_user_list = ProjectUser.objects.filter(user=user) # User maybe assigned to more than 1 project
 
-                logger.warn("User not in project of allocation")
-                # new_p_user = ProjectUser.objects.create(
-                #     project=a_user.allocation.project,
-                #     user=user,
-                #     status=ProjectUserStatusChoice.objects.get(name="Active"),
-                #     role=ProjectUserRoleChoice.objects.get(name="User"),
-                #     enable_notifications = True
-                # )
+            is_broken = True
+            for p_user in p_user_list:
+                if p_user.project == project:
+                    is_broken = False
+                    break
 
-        # Add to project
+            if is_broken:
+                if options["apply"]:
+                    new_p_user = ProjectUser.objects.create(
+                        project=a_user.allocation.project,
+                        user=user,
+                        status=ProjectUserStatusChoice.objects.get(name="Active"),
+                        role=ProjectUserRoleChoice.objects.get(name="User"),
+                        enable_notifications = True
+                    )
+                    logger.warn(f"Added User {user.username} to project '{project.title}'")
+                else:
+                    logger.warn(f"User {user.username} assgined to allocation pk = {a_user.allocation.pk}, but not in allocation's project '{project.title}'")
