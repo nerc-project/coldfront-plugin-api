61128ec57c4e3ce87c8bd6a87d0ab189e8640245
Quan Pham <74226479+QuanMPhm@users.noreply.github.com>
2023-12-20 12:55:21 -0500

    Implemented management command to fix database inconsistency (#18)
    I implemented a management command named fix_allocation_users that adds users to the projects that their assigned allocations are a part of. Running the command by itself only logs out which user has this database inconsistency problem, and using --apply fixes those inconsistencies.
    
    The test_fix_allocation test is added to test for this command.

---
+++ src/coldfront_plugin_api/management/commands/fix_allocation_users.py
@@ -0,0 +1,51 @@
+from django.core.management.base import BaseCommand
+from coldfront.core.allocation.models import AllocationUser
+from coldfront.core.project.models import (
+    Project,
+    ProjectUser,
+    ProjectUserStatusChoice,
+    ProjectUserRoleChoice,
+)
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = """Ensures all allocation users belong to their allocation's project.
+    Run with no arguements to only see which allocation users will be added to projects"""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--apply",
+            action="store_true",
+            help="Applies changes to database by adding allocation users to projects",
+        )
+
+    def handle(self, *args, **options):
+        # Check all allocation users
+        for a_user in AllocationUser.objects.filter(status__name="Active"):
+            user = a_user.user
+            project = a_user.allocation.project
+            p_user = ProjectUser.objects.filter(
+                user=user, project=project
+            )  # User maybe assigned to more than 1 project
+
+            if not p_user:
+                if options["apply"]:
+                    new_p_user = ProjectUser.objects.create(
+                        project=project,
+                        user=user,
+                        status=ProjectUserStatusChoice.objects.get(name="Active"),
+                        role=ProjectUserRoleChoice.objects.get(name="User"),
+                        enable_notifications=True,
+                    )
+                    logger.warn(
+                        f"Added User {user.username} to project '{project.title}'"
+                    )
+                else:
+                    logger.warn(
+                        f"User {user.username} assgined to allocation pk = {a_user.allocation.pk}, but not in allocation's project '{project.title}'"
+                    )
---
+++ src/coldfront_plugin_api/tests/unit/test_fix_allocations.py
@@ -0,0 +1,43 @@
+from django.core.management import call_command
+
+from coldfront.core.allocation.models import (
+    AllocationUser,
+    Allocation,
+    AllocationUserStatusChoice,
+)
+from coldfront.core.project.models import ProjectUser
+from coldfront_plugin_api.tests.unit import base
+
+from unittest.mock import patch
+
+
+class TestFixAllocation(base.TestBase):
+    @patch("coldfront_plugin_api.management.commands.fix_allocation_users.logger")
+    def test_command_output(self, mock_logger):
+        call_command("fix_allocation_users")
+        self.assertEqual(0, mock_logger.warn.call_count)
+
+        user = self.new_user()
+        allocation = Allocation.objects.first()
+
+        # Test with inconsistency
+        AllocationUser.objects.create(
+            user=user,
+            allocation=allocation,
+            status=AllocationUserStatusChoice.objects.get(name="Active"),
+        )
+
+        call_command("fix_allocation_users")
+        call_args = mock_logger.warn.call_args.args
+        self.assertIn(user.username, call_args[0])
+        self.assertIn("pk = " + str(allocation.pk), call_args[0])
+
+        # Test for project that user is not assigned to it
+        project = allocation.project
+        p_user = ProjectUser.objects.filter(project=project).filter(user=user)
+        self.assertFalse(p_user.exists())
+
+        # Test --apply
+        call_command("fix_allocation_users", "--apply")
+        p_user = ProjectUser.objects.filter(project=project).filter(user=user)
+        self.assertTrue(p_user.exists())
