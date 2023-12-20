c0ac3a2d8899f56a94c179590587180f070ddb42
Quan Pham <qmpham2019@gmail.com>
2023-12-20 11:42:56 -0500

    Cleaned fix_allocation_users.py

--- src/coldfront_plugin_api/management/commands/fix_allocation_users.py
+++ src/coldfront_plugin_api/management/commands/fix_allocation_users.py
@@ -1,48 +1,51 @@
 from django.core.management.base import BaseCommand
 from coldfront.core.allocation.models import AllocationUser
-from coldfront.core.project.models import Project, ProjectUser, ProjectUserStatusChoice, ProjectUserRoleChoice
+from coldfront.core.project.models import (
+    Project,
+    ProjectUser,
+    ProjectUserStatusChoice,
+    ProjectUserRoleChoice,
+)
 
 import logging
 
+
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
-    help = "A command that adds users who have been assigned allocations to those allocations' project. Made to fix a \
-    database inconsistency where a user is assigned to an allocation, but may not have been assigned to the allocation's project. \
-    Run this command with no options to see the list of users with this inconsistency. Run with '--apply' to fix them."
+    help = """Ensures all allocation users belong to their allocation's project.
+    Run with no arguements to only see which allocation users will be added to projects"""
 
     def add_arguments(self, parser):
         parser.add_argument(
             "--apply",
-            action="store_true", 
-            help="Applies changes to database to fix all allocation user inconsistencies"
+            action="store_true",
+            help="Applies changes to database by adding allocation users to projects",
         )
-        return 0
-    
+
     def handle(self, *args, **options):
-        
         # Check all allocation users
         for a_user in AllocationUser.objects.filter(status__name="Active"):
-
             user = a_user.user
             project = a_user.allocation.project
-            p_user_list = ProjectUser.objects.filter(user=user) # User maybe assigned to more than 1 project
+            p_user = ProjectUser.objects.filter(
+                user=user, project=project
+            )  # User maybe assigned to more than 1 project
 
-            is_broken = True
-            for p_user in p_user_list:
-                if p_user.project == project:
-                    is_broken = False
-                    break
-
-            if is_broken:
+            if not p_user:
                 if options["apply"]:
                     new_p_user = ProjectUser.objects.create(
-                        project=a_user.allocation.project,
+                        project=project,
                         user=user,
                         status=ProjectUserStatusChoice.objects.get(name="Active"),
                         role=ProjectUserRoleChoice.objects.get(name="User"),
-                        enable_notifications = True
+                        enable_notifications=True,
                     )
-                    logger.warn(f"Added User {user.username} to project '{project.title}'")
+                    logger.warn(
+                        f"Added User {user.username} to project '{project.title}'"
+                    )
                 else:
-                    logger.warn(f"User {user.username} assgined to allocation pk = {a_user.allocation.pk}, but not in allocation's project '{project.title}'")
+                    logger.warn(
+                        f"User {user.username} assgined to allocation pk = {a_user.allocation.pk}, but not in allocation's project '{project.title}'"
+                    )
