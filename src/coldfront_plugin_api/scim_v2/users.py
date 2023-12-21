5094ac260f911de4d148d4362f63a064e05a2845
Quan Pham <74226479+QuanMPhm@users.noreply.github.com>
2023-12-21 12:45:46 -0500

    Update users.py

--- src/coldfront_plugin_api/scim_v2/users.py
+++ src/coldfront_plugin_api/scim_v2/users.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
