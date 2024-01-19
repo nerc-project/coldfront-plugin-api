4577e82e2d32725733e9f25280245a1748df6d6d
Kristi Nikolla <kristi@nikolla.me>
2024-01-19 15:51:48 -0500

    Improvements to reliability
    - Added a small 0.5 seconds wait between students.
    - Added reauthentication every 50 students to prevent session
      expiration.
    - Added error logging and exit on error.
    - Used the click library and updated help.
    - Added a --add-to-rhods-notebooks-namespace flag for when
      registering TAs.

--- tools/register_users_from_csv.py
+++ tools/register_users_from_csv.py
@@ -1,26 +1,34 @@
 """
-Create and register users into an allocation.
+Usage: register_users_from_csv.py [OPTIONS]
 
-Usage:
-    python3 register_users_from_csv.py <input.csv> <scim_v2_base_url>
+  Create and register users into an allocation.
 
-Configuration:
-- The format of the CSV is:
-username,firstName,lastName,allocationId
+  Environment variables CLIENT_ID and CLIENT_SECRET must be set, corresponding
+  to a service account in Keycloak. (ex. xdmod-client)
 
-- Environment variables CLIENT_ID and CLIENT_SECRET must be set,
-corresponding to a service account in Keycloak.
+Options:
+  --csv-file PATH                 The format of the CSV is:
+                                  username,firstName,lastName,allocationId
+  --add-to-rhods-notebooks-namespace
+                                  Add users to the `rhods-notebooks` namespace
+                                  (TAs/Profs). Requires `oc` command and
+                                  cluster admin access to OpenShift.
+  --help                          Show this message and exit.
+
 """
 import csv
 import logging
 import os
 import re
 import sys
+import time
 
+import click
 import requests
 from requests.auth import HTTPBasicAuth
 
-logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ScimClient(object):
@@ -28,19 +36,19 @@ class ScimClient(object):
         self, scim_url, keycloak_url, keycloak_client_id, keycloak_client_secret
     ):
         self.scim_url = scim_url
-        self.session = self.get_session(
-            keycloak_url, keycloak_client_id, keycloak_client_secret
-        )
+        self.keycloak_url = keycloak_url
+        self.keycloak_client_id = keycloak_client_id
+        self.keycloak_client_secret = keycloak_client_secret
+        self.session = self.refresh_session()
 
-    @staticmethod
-    def get_session(keycloak_url, keycloak_client_id, keycloak_client_secret):
+    def refresh_session(self):
         """Authenticate as a client with Keycloak to receive an access token."""
-        token_url = f"{keycloak_url}/auth/realms/mss/protocol/openid-connect/token"
+        token_url = f"{self.keycloak_url}/auth/realms/mss/protocol/openid-connect/token"
 
         r = requests.post(
             token_url,
             data={"grant_type": "client_credentials"},
-            auth=HTTPBasicAuth(keycloak_client_id, keycloak_client_secret),
+            auth=HTTPBasicAuth(self.keycloak_client_id, self.keycloak_client_secret),
         )
         client_token = r.json()["access_token"]
 
@@ -50,6 +58,8 @@ class ScimClient(object):
             "Content-Type": "application/json",
         }
         session.headers.update(headers)
+
+        logger.info("Authenticated with Keycloak")
         return session
 
     def create_user(self, username, first_name, last_name, email):
@@ -78,6 +88,11 @@ class ScimClient(object):
             r = self.session.post(f"{self.scim_url}/Users", json=payload)
             if r.status_code == 201:
                 print(f"Created user {username}.")
+            else:
+                logger.error(
+                    f"Error creating user {username}: {r.status_code}: {r.text}"
+                )
+                sys.exit(1)
 
     def add_user_to_group(self, username, allocation):
         r = self.session.get(f"{self.scim_url}/Groups/{allocation}")
@@ -93,6 +108,9 @@ class ScimClient(object):
             r = self.session.patch(f"{self.scim_url}/Groups/{allocation}", json=payload)
             if r.status_code in [200, 201]:
                 print(f"Added user {username} to allocation {allocation}.")
+            else:
+                logger.error(f"Error adding user {username}: {r.status_code}: {r.text}")
+                sys.exit(1)
 
 
 def get_sanitized_name(name):
@@ -110,29 +128,57 @@ def get_sanitized_name(name):
     return name
 
 
-def main():
+@click.command(
+    help=(
+        "Create and register users into an allocation.\n\n"
+        "Environment variables CLIENT_ID and CLIENT_SECRET must be set, "
+        "corresponding to a service account in Keycloak. (ex. xdmod-client)"
+    )
+)
+@click.option(
+    "--csv-file",
+    type=click.Path(),
+    help="The format of the CSV is: username,firstName,lastName,allocationId",
+)
+@click.option(
+    "--add-to-rhods-notebooks-namespace",
+    is_flag=True,
+    default=False,
+    help=(
+        "Add users to the `rhods-notebooks` namespace (TAs/Profs). "
+        "Requires `oc` command and cluster admin access to OpenShift."
+    ),
+)
+def main(csv_file, add_to_rhods_notebooks_namespace):
     client = ScimClient(
-        sys.argv[2],
+        "https://coldfront.mss.mghpcc.org/api/scim/v2",
         "https://keycloak.mss.mghpcc.org",
         os.environ.get("CLIENT_ID"),
         os.environ.get("CLIENT_SECRET"),
     )
 
-    with open(sys.argv[1], newline="") as csvfile:
+    with open(csv_file, newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=",", quotechar="|")
-        for row in reader:
-            print(f"Processing: {row}.")
+        for index, row in enumerate(reader):
+            logger.info(f"Processing: {row}.")
 
             client.create_user(
                 username=row[0], first_name=row[1], last_name=row[2], email=row[0]
             )
             client.add_user_to_group(username=row[0], allocation=row[3])
 
-            # Add to rhods-notebook namespace
-            sanitized_name = get_sanitized_name(row[0])
-            os.system(
-                f"oc -n rhods-notebooks create rolebinding {sanitized_name} --clusterrole=edit --user={row[0]} --as system:admin"
-            )
+            if add_to_rhods_notebooks_namespace:
+                logger.info("Adding user to rhods notebooks.")
+                sanitized_name = get_sanitized_name(row[0])
+                os.system(
+                    f"oc -n rhods-notebooks create rolebinding {sanitized_name} --clusterrole=edit --user={row[0]} --as system:admin"
+                )
+
+            # Rate-limiting
+            time.sleep(0.5)
+
+            if index % 50 == 49:
+                client.session = client.refresh_session()
 
 
 if __name__ == "__main__":
