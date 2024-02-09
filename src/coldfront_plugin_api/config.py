c6b6f760d797efa8baaf3d8f0fb1e64434aabf2d
Quan Pham <qmpham2019@gmail.com>
2024-02-08 21:25:15 -0500

    urls.py: Replaced the views in scim_v2/users.py with the User view implemented by django_scim, as well as the root view.
    The application namespace 'scim' is also defined, based on the requirements of the django_scim module.

--- src/coldfront_plugin_api/config.py
+++ src/coldfront_plugin_api/config.py
@@ -1,6 +1,6 @@
 from coldfront.config.base import INSTALLED_APPS
 from coldfront.config.env import ENV
 
-for app in ['rest_framework', 'coldfront_plugin_api']:
+for app in ['rest_framework', 'coldfront_plugin_api', 'django_scim']:
     if app not in INSTALLED_APPS:
         INSTALLED_APPS.append(app)
