6d333f9c647e651404121de8cc822db2467be2b8
Quan Pham <qmpham2019@gmail.com>
2025-03-07 10:11:23 -0500

    Fixed unit and functional tests

--- ci/run_keycloak.sh
+++ ci/run_keycloak.sh
@@ -5,4 +5,4 @@ sudo docker run -d --name keycloak \
     -e KEYCLOAK_ADMIN_PASSWORD=nomoresecret \
     -p 8080:8080 \
     -p 8443:8443 \
-    quay.io/keycloak/keycloak:latest start-dev
+    quay.io/keycloak/keycloak:25.0 start-dev
